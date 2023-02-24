76af7e26adcd20b8ba34459239c04bbd7ee3258e
YaelGF <1719110736@utectulancingo.edu.mx>
2023-02-23 23:02:49 -0600

    Projects

--- Ejercicio.py
+++
@@ -1,8 +0,0 @@
-dato = input("Ingrese dato: ")
-
-lista = ['hola','mundo','chanchito','feliz','dragones']
-
-if lista.count(dato) > 0:
-    print("El dato existe", dato)
-else:
-    print("El dato no existe", dato)
